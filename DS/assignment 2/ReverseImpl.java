386b2cc7e84b560557ee2b5e2889d28ec85804d8
Appa07 <81469915+Appa07@users.noreply.github.com>
2023-05-31 01:12:53 +0530

    Add files via upload

---
+++ DS/assignment 2/ReverseImpl.java
@@ -0,0 +1,17 @@
+import ReverseModule.ReversePOA;
+import java.lang.String;
+import java.io.*;
+class ReverseImpl extends ReversePOA
+{
+ReverseImpl()
+{
+super();
+System.out.println("Reverse Object Created");
+}
+public String reverse_string(String name)
+{
+StringBuffer str=new StringBuffer(name);
+str.reverse();
+return (("Server Send "+str));
+}
+}
